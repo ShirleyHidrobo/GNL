f68d3be322ad5ac824a405a3211f271faef367f1
Shirley Hidrobo Cordova <shhidrob@c1r4s4.42london.com>
2025-03-30 20:58:51 +0100

    mandatory all good

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shhidrob <shhidrob@student.42london.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/09 11:38:21 by shhidrob          #+#    #+#             */
+/*   Updated: 2025/03/30 20:11:06 by shhidrob         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+char	*get_next_line(int fd);
+char	*readbuff(int fd, char *buff, char *storage);
+char	*extract_line(char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strndup(char *src, size_t n);
+void	*ft_memmove(void *dest, void *src, size_t n);
+char	*ft_strdup(char *src);
+char	*ft_strnjoin(char *s1, char *s2, size_t n);
+char	*ft_strchr(char *s, int c);
+
+#endif
