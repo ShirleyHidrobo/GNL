af63ce2ef9098a0dd849cd401d6ed8257894b20a
Shirley Hidrobo Cordova <shhidrob@c1r4s4.42london.com>
2025-03-30 21:08:23 +0100

    b

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shhidrob <shhidrob@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 17:55:33 by shhidrob          #+#    #+#             */
-/*   Updated: 2025/03/30 20:56:40 by shhidrob         ###   ########.fr       */
+/*   Updated: 2025/03/30 21:05:31 by shhidrob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,30 +87,30 @@ char	*get_next_line(int fd)
 	return (storage);
 }
 
-// int main(int argc, char **argv)
-// {
-//     int fd;
-//     char *line;
+int main(int argc, char **argv)
+{
+    int fd;
+    char *line;
 
-//     if (argc == 2)
-//     {
-// 	printf("Opening file: %s\n", argv[1]);
-// 	fd = open(argv[1], O_RDONLY);
-// 	if (fd == -1)
-// 	    return (1);
-// 	printf("File opened\n");
-// 	line = get_next_line(fd);
-// 	while (line)
-// 	{
-// 	    printf("%s", line);
-// 	    free(line);
-// 	    line = get_next_line(fd);
-// 	}
-// 	close(fd);
-// 	printf("File now closed\n");
-//     }
-//     return (0);
-// }
+    if (argc == 2)
+    {
+	printf("Opening file: %s\n", argv[1]);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	    return (1);
+	printf("File opened\n");
+	line = get_next_line(fd);
+	while (line)
+	{
+	    printf("%s", line);
+	    free(line);
+	    line = get_next_line(fd);
+	}
+	close(fd);
+	printf("File now closed\n");
+    }
+    return (0);
+}
 
 // int main(int argc, char **argv)
 // {
